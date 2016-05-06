fbf1d4c3e14655fb4fb668e87091b8dad7b46260
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2016-05-06 19:04:06 -0400

    Added a simple header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = 'Victor Sagrista Lopez';
+var formattedRole = 'Software Developer';
+HTMLheaderName= HTMLheaderName.replace('%data%',formattedName);
+HTMLheaderRole = HTMLheaderRole.replace('%data%',formattedRole);
+$('#header').prepend(HTMLheaderName);
+$('#header').prepend(HTMLheaderRole);
